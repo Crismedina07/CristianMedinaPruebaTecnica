4d71f0c28f9a9927f3da141ed551f2450be05b03
Crismedina07 <41929688+Crismedina07@users.noreply.github.com>
2024-03-18 22:57:12 -0400

    Initial Commit

---
+++ SRC/contribuyentesWebAPI/contribuyentesWebAPI/Models/Contribuyentes.cs
@@ -0,0 +1,10 @@
+﻿namespace contribuyentesWebAPI.Models
+{
+    public class Contribuyentes
+    {
+        public int rncCedula { get; set; }
+        public string Nombre { get; set; }  
+        public string Tipo { get; set; }
+        public string Estatus { get; set; }
+    }
+}
---
+++ SRC/contribuyentesWebAPI/contribuyentesWebAPI/Models/dbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace contribuyentesWebAPI.Models
+{
+    public class dbContext: DbContext
+    {
+        public dbContext(DbContextOptions<dbContext> options) : base(options) { }
+        public DbSet<Contribuyentes>Contribuyentes { get; set; }
+    }
+}
